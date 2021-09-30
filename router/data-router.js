99c28a7333e8a91e06c1f1ee7f64d5b478307e76
keshawn0998 <keshawn@contenda.co>
2021-09-30 09:07:14 -0400

    changed to dion's email

--- router/data-router.js
+++ router/data-router.js
@@ -293,7 +293,7 @@ router.post('/email/:business_name', (req, res) => {
   console.log(req.body)
   let message = {
     from: "diondetailingemailbot@gmail.com",
-    to: "diondetailingemailbot@gmail.com",
+    to: "dows6726@gmail.comCH",
     subject:req.body.subject,
     text: req.body.message,
     html: req.body.message
