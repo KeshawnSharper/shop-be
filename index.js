102392878664772ce97eaaa997c3e609fed15a58
KESHAWN SHARPER <ksharper@studentmba.org>
2024-08-15 22:01:49 -0400

    cors1

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const server = require('./server.js');
-
+const cors = require("cors")
+const corsOptions = {
+  credentials: true,
+  origin: "*"// Whitelist the domains you want to allow
+};
 const PORT = process.env.PORT || 5000;
 require('dotenv').config()
+server.use(cors(corsOptions));
 
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
